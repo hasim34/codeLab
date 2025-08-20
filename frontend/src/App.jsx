d30255fb2724926580300b1655b8642d03670975
Hasan Hasim <hasim30042005@gmail.com>
2025-08-20 17:17:42 +0530

    208

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import Layout from "./layouts/Layout";
+
 function App() {
   return (
-    <div>
-      <h1>CodeLab </h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={Login} />
+        <Route path="/register" element={Register} />
+
+        <Route path="/dashboard" element={Layout}>
+          <Route index element/>        
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
